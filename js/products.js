c68e4a21d01d37c83fb543d86441991e9ec5779a
rohant20 <59935451+rohant20@users.noreply.github.com>
2021-01-20 18:52:46 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,20 +1,81 @@
+window.addEventListener("scroll", function () {
+	var navbar = document.getElementById("nav")
+	navbar.classList.toggle("bg-dark", window.scrollY > 0)
+})
+
+function opaque(navbar) {
+	navbar.classList.toggle("bg-dark", true)
+}
+
+function transparent(navbar) {
+	navbar.classList.toggle("bg-dark", window.scrollY > 0 && !($('#nav').is(':hover')))
+}
+
+$('.dropdown').on('show.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+$('.dropdown').on('hide.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
+
+function dropDownOpen(dropdown) {
+	$(dropdown).dropdown('show')
+
+}
+
+function dropDownClose(dropdown) {
+	$(dropdown).dropdown('hide')
+}
 
 
-function hoverCover(card) {
-    var image1 = document.getElementById("img1")
-    var image2 = document.getElementById("img2")
-    var image3 = document.getElementById("img3")
-    var image4 = document.getElementById("img4")
-    if(card.id == "img1") {
-        image1.style.opacity = "0"
-        
-    } else if(card.id == "img2") {
-        console.log("It's working");
-    }
-    
+
+function changeVideo1() {
+	var btn1 = document.getElementById("btn1")
+	btn1.classList.toggle("bg-dark", true)
+	var source = document.createElement("source")
+	var videoSource = document.getElementById("video1")
+	videoSource.style.visibility = "hidden"
 };
 
-function offCover(card, image1, image2, image3, image4) {
-    image1.style.opacity = "1";
-    console.log("It's working");
+function carouselAni(button) {
+	var displaying = 0
+	button.classList.toggle("bg-dark", true)
+	var review1 = document.getElementById("review1")
+	var review2 = document.getElementById("review2")
+	var review3 = document.getElementById("review3")
+	var dim = button.getBoundingClientRect();
+	var limit = review3.getBoundingClientRect();
+	var a = dim.right
+	var b = limit.right
+	var c = b - a
+	var i;
+	if(button.id == "review1"){
+		review2.style.opacity = "0"
+		review3.style.opacity = "0"
+		var displaying = 1
+	} else if (button.id == "review2") {
+		review1.style.opacity = "0"
+		review3.style.opacity = "0"
+		var displaying = 2
+	} else {
+		review2.style.opacity = "0"
+		review1.style.opacity = "0"
+		var displaying = 3
+	};
+	if (displaying == 1) {
+
+	} else if(displaying == 2) {
+
+	} else if (displaying == 3){
+
+	} else {
+		console.log("Something is wrong!")
+	};
+	$(button).animate({left: c + 'px'});
+	var para = document.createElement("h4")
+
+
 };
+
+//document.getElementsByClassName("dropdown-menu").addEventListener("wheel", dropDownClose);
