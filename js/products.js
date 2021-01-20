89f0db637f7e7fec3f27da32dd4d077955e4295e
tsa-webmaster <karthiktaranath@gmail.com>
2021-01-19 22:12:09 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -8,13 +8,13 @@ function hoverCover(card) {
     if(card.id == "img1") {
         image1.style.opacity = "0"
         
-    } else if(card.id == "img2")
-    console.log("It's working")
-
+    } else if(card.id == "img2") {
+        console.log("It's working");
+    }
     
 };
 
 function offCover(card, image1, image2, image3, image4) {
-    image1.style.opacity = "1"
-    console.log("It's working")
+    image1.style.opacity = "1";
+    console.log("It's working");
 };
