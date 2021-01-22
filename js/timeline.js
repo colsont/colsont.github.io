10985081d56a4f9bdbd240836f249ec650e67bdb
tsa-webmaster <karthiktaranath@gmail.com>
2021-01-22 18:06:31 -0500

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,8 +1,14 @@
-jQuery(function ($) {
-    $('#slides').fullpage({
-        menu: '#menu',
-        slidesNavigation: true,
-        scrollHorizontally: true
-    });
+var deleteLog = false;
 
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        navigation: {
+            'textColor': '#f2f2f2',
+            'bulletsColor': '#ccc',
+            'position': 'right',
+        },
+        loopTop: true,
+        loopBottom: true
+    });
 });
